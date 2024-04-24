e7bfc1f1b9d5f5c6287bd3be4b2f4227775c7c11
Raghuvansh <raghuvansh.siddharth004@gmail.com>
2024-04-24 22:58:58 +0530

    added a comment

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("Path Finding Visualizer")
 
+#define colours
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
